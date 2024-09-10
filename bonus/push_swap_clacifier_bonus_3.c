0fbfd9fd1baedba1971ee659e4d61131d10a6bdb
NdondaDaniel2020 <root@Spot.>
2024-09-10 20:10:29 +0100

    organizando o mandatorio

---
+++ bonus/push_swap_clacifier_bonus_3.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap_clacifier_bonus_3.c                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmatondo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/16 14:23:41 by nmatondo          #+#    #+#             */
+/*   Updated: 2024/08/08 08:13:36 by nmatondo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	rra(t_data *data, int i)
+{
+	t_list	*aux;
+	t_list	*last;
+
+	if (ft_lstsize(data->stack_a) > 1)
+	{
+		aux = data->stack_a;
+		while (aux->next->next)
+			aux = aux->next;
+		last = aux->next;
+		last->next = data->stack_a;
+		aux->next = NULL;
+		data->stack_a = last;
+		if (i == 1)
+			write(1, "rra\n", 4);
+	}
+}
+
+void	rrb(t_data *data, int i)
+{
+	t_list	*aux;
+	t_list	*last;
+
+	if (ft_lstsize(data->stack_b) > 1)
+	{
+		aux = data->stack_b;
+		while (aux->next->next)
+			aux = aux->next;
+		last = aux->next;
+		last->next = data->stack_b;
+		aux->next = NULL;
+		data->stack_b = last;
+		if (i == 1)
+			write(1, "rrb\n", 4);
+	}
+}
+
+void	rrr(t_data *data)
+{
+	rra(data, 0);
+	rrb(data, 0);
+	write(1, "rrr\n", 4);
+}
---
+++ bonus/push_swap_utils_3.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap_utils_3.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmatondo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/16 14:23:41 by nmatondo          #+#    #+#             */
+/*   Updated: 2024/08/08 08:13:36 by nmatondo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	error(t_data *data)
+{
+	write(2, "Error\n", 6);
+	clean_stack(data);
+	exit(1);
+}
+
+static char	*fun_number_max(void)
+{
+	return ("2147483647");
+}
+
+void	check_size_number(t_data *data, char *num_in_char)
+{
+	int		i;
+	int		ix;
+	char	*number_max;
+
+	i = 0;
+	ix = 0;
+	number_max = fun_number_max();
+	if ((ft_strlen(num_in_char) == 10)
+		|| (ft_strlen(num_in_char) == 11 && num_in_char[0] == '-'))
+	{
+		if (ft_strlen(num_in_char) == 11 && num_in_char[0] == '-')
+			i++;
+		while (num_in_char[i])
+		{
+			if (num_in_char[i] > number_max[ix])
+				error(data);
+			i++;
+			ix++;
+		}
+	}
+	else if (ft_strlen(num_in_char) > 10)
+		error(data);
+}
+
+void	check_is_number(t_data *data, char *num_in_char)
+{
+	int	i;
+
+	i = 0;
+	while (num_in_char[i])
+	{
+		if (!ft_isdigit(num_in_char[i]))
+		{
+			if ((num_in_char[i] == '-' || num_in_char[i] == '+')
+				&& (i == 0 || num_in_char[i - 1] == ' ')
+				&& ft_isdigit(num_in_char[i + 1]))
+				ft_printf("");
+			else
+				error(data);
+		}
+		i++;
+	}
+}
